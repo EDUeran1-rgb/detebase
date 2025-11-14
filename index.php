55da5e73e6e24e5e85399feef0c361f2ee12f6f7
EDUeran1-rgb <EDUeran1@utb.karlskoga.se>
2025-11-14 11:16:33 +0100

    j

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $db="dadabas";
+    $conn=mysqli_connect($host, $user, $pass, $db);
+    if(isset($_POST['btn'])){
+        $joketext=$_POST['joketext'];
+        $jokeanswer=$_POST['jokeanswer'];
+        $sql="INSERT INTO jokes(joketext, jokeanswer) VALUES('$joketext','$jokeanswer')";
+        $result=mysqli_query($conn,$sql);
+        
+    }
+    $sql="SELECT * FROM jokes ORDER BY score DESC";
+    $result=mysqli_query($conn,$sql);
+    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Detebase</h1>
+    </header>
+    <main>
+        <section>
+            <form action="index.php" method="post">
+                <label> Pappaskämpet 
+                    <textarea name="joketext"></textarea>
+                </label>
+                <label>Svaret på skämtet
+                    <input type="text" name="jokeanswer">
+                </label>
+                <input type="submit" value="Lägg in" name="btn">
+            </form>
+        </section>
+        <section>
+            <?php while($row=mysqli_fetch_assoc($result)): ?>
+                <details><summary><?=$row['joketext']?></summary><?=$row['joketext']?></details>
+            <?php endwhile ?>
+        </section>
+    </main>
+    <footer>&copy; all dads forever 2025</footer>
+</body>
+</html>
